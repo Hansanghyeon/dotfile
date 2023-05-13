153e7b13d4480d81b7c8da3d6a3c4cf80493875f
Hansanghyeon <999@hyeon.pro>
2023-05-13 14:07:20 +0900

    feat: telescope에 필요한 플러그인 추가

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,6 +1,15 @@
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
+  -- general dev
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
+  }
+
   -- search
   use { 'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/plenary.nvim'}} }
 
