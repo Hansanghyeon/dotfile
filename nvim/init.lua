62f4e611e73fd96de5e3e84e5ff14dffcabcb689
999 <999@hyeon.pro>
2025-02-09 11:43:10 +0900

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,15 +1,11 @@
 require('plugins')
 require('mappings')
 
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "lua",
-	callback = function()
-		vim.opt_local.tabstop = 2
-		vim.opt_local.shiftwidth = 2
-		vim.opt_local.expandtab =true
-		vim.opt_local.softtabstop = 2
-	end
-})
+local set = vim.opt
 
+set.tabstop = 2
+set.shiftwidth = 2
+set.softtabstop = 2
+set.expandtab = true
 -- 내 서버 vim에서는 항상 preview가 나오고 ui가 깨지는 현상이있다. 해당 옵션으로 문제를 해결할 수 있음
-vim.o.pumblend = 0
+set.pumblend = 0
